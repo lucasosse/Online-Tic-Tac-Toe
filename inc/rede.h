cb3785767628512d61dd3c64e4a193f66d278345
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2016-12-23 21:18:00 -0200

    Added necessary functions signatures

--- inc/rede.h
+++ inc/rede.h
@@ -1,4 +1,10 @@
 #ifndef REDE_H
     #define REDE_H
-    
+        #include "tabuleiro.h"
+
+        int socket_host_game(const char *host_address);
+        int socket_connect(const char *host_address);
+        int socket_send(int *socket_fd, const char *to_address, unsigned short x, unsigned short y);
+        int socket_receive(int *socket_fd, const char *from_address);
+        void socket_destroy(int *socket_fd);
 #endif
