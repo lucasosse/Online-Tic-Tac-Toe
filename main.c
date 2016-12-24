a4ec13289233b694c45214e95a67b8c310b4bc6d
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2016-12-23 23:37:16 -0200

    Modified to test network interface

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "inc/menu.h"
+#include "inc/rede.h"
 
 int main(int argc, char *argv[])
 {
+    int sock = -1;
+    if(argv[1][0] == '1')
+    {
+        sock = socket_host_game("127.0.0.1");
+        socket_receive(&sock, "127.0.0.1");
+    }
+    else
+    {
+        sock = socket_connect("127.0.0.1");
+        socket_send(&sock, "127.0.0.1", 0, 0);
+    }
+    if(sock != -1) printf("SUCCESS!\n");
+    else printf("ERROR!\n");
+
+    socket_destroy(&sock);
     return 0;
 }
