0894ef66d0a490ca644e6b910179a1dbcd8a6ec3
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2017-01-11 03:55:24 -0200

    Modified to work in Windows operatig systems

--- inc/rede.h
+++ inc/rede.h
@@ -2,9 +2,20 @@
     #define REDE_H
         #include "tabuleiro.h"
 
-        int socket_host_game(const char *host_address);
-        int socket_connect(const char *host_address);
-        int socket_send(int *socket_fd, const char *to_address, unsigned short x, unsigned short y);
-        int socket_receive(int *socket_fd, const char *from_address);
-        void socket_destroy(int *socket_fd);
+        #ifdef _WIN32
+            #include <winsock2.h>
+            #include <ws2tcpip.h>
+
+            SOCKET socket_host_game(const char *address);
+            SOCKET socket_connect_game(const char *address);
+            int socket_send(SOCKET *socket_fd, const char *to_address, unsigned short x, unsigned short y);
+            int socket_receive(SOCKET *socket_fd, const char *from_address);
+            void socket_destroy(SOCKET *socket_fd);
+        #else
+            int socket_host_game(const char *address);
+            int socket_connect_game(const char *address);
+            int socket_send(int *socket_fd, const char *to_address, unsigned short x, unsigned short y);
+            int socket_receive(int *socket_fd, const char *from_address);
+            void socket_destroy(int *socket_fd);
+        #endif
 #endif
