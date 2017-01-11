f62470c54ba0f367fbf4dac06e5e595670ac3d7b
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2017-01-11 16:41:53 -0200

    Modified receive function

--- inc/rede.h
+++ inc/rede.h
@@ -9,13 +9,13 @@
             SOCKET socket_host_game(const char *address);
             SOCKET socket_connect_game(const char *address);
             int socket_send(SOCKET *socket_fd, const char *to_address, unsigned short x, unsigned short y);
-            int socket_receive(SOCKET *socket_fd, const char *from_address);
+            int socket_receive(SOCKET *socket_fd, const char *from_address, char *recv_buffer);
             void socket_destroy(SOCKET *socket_fd);
         #else
             int socket_host_game(const char *address);
             int socket_connect_game(const char *address);
             int socket_send(int *socket_fd, const char *to_address, unsigned short x, unsigned short y);
-            int socket_receive(int *socket_fd, const char *from_address);
+            int socket_receive(int *socket_fd, const char *from_address, char *recv_buffer);
             void socket_destroy(int *socket_fd);
         #endif
 #endif
